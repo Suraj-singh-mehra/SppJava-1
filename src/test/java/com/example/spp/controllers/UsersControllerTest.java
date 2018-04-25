11286cdd8a34453c1afbac56997e6aacd272d9fc
LisavetaTsyulia <tsyulialisa@gmail.com>
2018-04-25 07:53:20 +0300

    Second test works for User Controller

--- src/test/java/com/example/spp/controllers/UsersControllerTest.java
+++ src/test/java/com/example/spp/controllers/UsersControllerTest.java
@@ -1,7 +1,12 @@
 package com.example.spp.controllers;
 
+import com.example.spp.dto.AddNewUserDto;
 import com.example.spp.models.User;
 import com.example.spp.repository.UserRepository;
+import com.example.spp.service.AuthenticationService;
+import com.example.spp.service.TestDBUsersService;
+import com.example.spp.service.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
@@ -14,6 +19,7 @@ import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilde
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.when;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -27,6 +33,18 @@ public class UsersControllerTest extends BaseControllerTest{
     @MockBean
     private UserRepository userRepository;
 
+    @Autowired
+    private ObjectMapper mapper;
+
+    @Autowired
+    private TestDBUsersService testDBUsersService;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private AuthenticationService authenticationService;
+
     @Before
     public void setupMock() {
         User user1 = new User();
@@ -38,6 +56,7 @@ public class UsersControllerTest extends BaseControllerTest{
         users.add(user2);
         when(userRepository.findAll())
                 .thenReturn(users);
+        when(userRepository.save(any(User.class))).thenReturn(null);
     }
 
     @Test
@@ -51,5 +70,26 @@ public class UsersControllerTest extends BaseControllerTest{
                 .andExpect(jsonPath("$[0].email", Matchers.equalTo("first@email.com")))
                 .andExpect(jsonPath("$[1].email", Matchers.equalTo("second@email.com")));
     }
+
+    @Test
+    public void addNewUser() throws Exception {
+        AddNewUserDto addNewUserDto = new AddNewUserDto();
+        addNewUserDto.setStatus("ACTIVE");
+        addNewUserDto.setSalary(1000);
+        addNewUserDto.setRole("ROLE_ACCOUNTER");
+        addNewUserDto.setFullname("Some User");
+        addNewUserDto.setEmail("some@email.com");
+        addNewUserDto.setId(1);
+
+        String json = mapper.writeValueAsString(addNewUserDto);
+        MockHttpServletRequestBuilder requestBuilder = post("/addNewUser")
+                .content(json)
+                .contentType(MediaType.APPLICATION_JSON);
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", Matchers.equalTo(true)));
+    }
+
+
 
 }
